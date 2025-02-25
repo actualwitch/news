c6c0bedf975df3793504064d3672c408f461c161
Adelaide あで Fisher <noreply@actualwitch.me>
2025-02-25 23:40:48 +0100

    structs and such

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,25 @@
+use chrono::{DateTime, FixedOffset};
 use leptos::prelude::*;
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
     components::{Route, Router, Routes},
     StaticSegment,
 };
+use serde::{Deserialize, Serialize};
+use typed_builder::TypedBuilder;
+use url::Url;
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, TypedBuilder)]
+pub struct Story {
+    title: String,
+    #[builder(default, setter(strip_option))]
+    text: Option<String>,
+    #[builder(default, setter(strip_option))]
+    url: Option<Url>,
+    created_at: DateTime<FixedOffset>,
+    #[builder(default, setter(strip_option))]
+    updated_at: Option<DateTime<FixedOffset>>,
+}
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
@@ -12,6 +28,7 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
             <head>
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" type="image/png" href="favicon.png" />
                 <AutoReload options=options.clone() />
                 <HydrationScripts options/>
                 <MetaTags/>
@@ -68,7 +85,18 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
+    let async_data = Resource::new(|| 0, move |_| get_news());
     view! {
         <div />
     }
+}
+
+#[server]
+pub async fn get_news() -> Result<Vec<Story>, ServerFnError> {
+    let timestamp = Local::now();
+    Ok(vec![Story::builder()
+        .title("🦀 React is dead 🦀".to_string())
+        .created_at(timestamp.into())
+        .url(Url::parse("https://www.rust-lang.org/")?)
+        .build()])
 }
